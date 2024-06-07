33996f6789d283acedd24c7e4a8269a808d42f7c
lilydjwg <lilydjwg@gmail.com>
2024-06-07 16:02:20 +0800

    wireplumber: support differentiate output devices by soundcard port

--- wireplumber/scripts/bose-eq.lua
+++ wireplumber/scripts/bose-eq.lua
@@ -2,11 +2,16 @@ lutils = require ("linking-utils")
 cutils = require ("common-utils")
 log = Log.open_topic ("s-linking")
 
-should_use_bose_eq = function(name)
+should_use_bose_eq = function(name, device_id)
+  log:debug("should_use_bose_eq: " .. name .. ", " .. tostring(device_id))
   if name == "alsa_output.pci-0000_0f_00.4.analog-stereo" then
     return true
   elseif name == "bluez_output.C8_7B_23_7F_7F_81.1" then
     return true
+  elseif name:match("alsa_output%.pci%-0000_00_1f%.3.*%.analog%-stereo") then
+    local port_name = get_device_port_name(device_id)
+    log:debug("port_name: " .. tostring(port_name))
+    return port_name == "analog-output-headphones"
   end
 end
 
@@ -33,6 +38,23 @@ find_bose = function(om)
   end
 end
 
+get_device_port_name = function(device_id)
+  if device_id then
+    local device = cutils.get_object_manager("device"):lookup {
+      type = "device",
+      Constraint { "object.id", "=", device_id },
+    }
+    if device then
+      for p in device:iterate_params("Route") do
+        local route = cutils.parseParam(p, "Route")
+        if route.direction == "Output" then
+          return route.name
+        end
+      end
+    end
+  end
+end
+
 SimpleEventHook {
   name = "linking/bose-eq",
   after = "linking/find-best-target",
@@ -48,6 +70,7 @@ SimpleEventHook {
     local target_props = target.properties
     local target_name = target_props["node.name"]
     local dont_move = cutils.parseBool(si_props["node.dont-move"])
+    local device_id = target_props["device.id"]
 
     log:info("name: " .. name .. " target: " .. target_props["node.name"])
 
@@ -55,13 +78,14 @@ SimpleEventHook {
       and not si_flags.has_defined_target
       and target_direction == "input"
       and name ~= "effect_output.bose_eq" then
-      if should_use_bose_eq(target_name) then
+      if should_use_bose_eq(target_name, device_id) then
         log:info("switched name: " .. name)
         event:set_data("target", find_bose_eq(om))
       end
     end
 
-    if name == "effect_output.bose_eq" and not should_use_bose_eq(target_name) then
+    if name == "effect_output.bose_eq"
+      and not should_use_bose_eq(target_name, device_id) then
       local t = find_bose(om)
       log:info("found bose: " .. tostring(t))
       if t then
